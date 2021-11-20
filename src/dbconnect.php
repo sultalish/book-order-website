6cc11d2a348ed212a42cb3b5ac007562dab3251b
Taoufik Laaroussi <mr.l.t@hotmail.com>
2021-11-19 22:48:02 -0500

    Add signup page and login for professors

---
+++ src/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+
+      $dbServerName = "localhost";
+      $dbUserName = "root";
+      $dbPassword = "toorqwer";
+      $dbName = "bookstore";
+
+      $con = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
