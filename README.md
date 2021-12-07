c5a7d5c132904a1c7f596c3157be27385e020285
rossrellis <54516944+rossrellis@users.noreply.github.com>
2021-12-06 21:11:03 -0500

    My Files to be Merged

--- README.md
+++
@@ -1,1 +0,0 @@
-# book-order-website
--- src/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-      $dbServerName = "localhost";
-      $dbUserName = "root";
-      $dbPassword = "";
-      $dbName = "bookstore";
-
-      $con = mysqli_connect($dbServerName, $dbUserName, $dbPassword,$dbName);
